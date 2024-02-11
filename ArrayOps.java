2d248fd049f554f6db2bf4a33dfdf251eceddf4d
DvirBendavid <dvir.bendavid@post.runi.ac.il>
2024-02-11 23:36:19 +0200

    added code

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,107 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int [] check =  {10,9,8,7,6,8};
+        System.out.println(isSorted(check));
     }
     
+    //added contains function.
+    public static boolean contains(int [] array , int value){
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == value)
+                return true;
+
+        }
+
+        return false;
+
+    }
+
+    public static void printArr(int[] arr){
+        String ans = "{";
+        for (int i = 0; i <arr.length ; i++) {
+            ans = (arr.length - 1 == i)? ans+ arr[i] + "}" :ans + arr[i] + ", ";
+        }
+        System.out.println(ans);
+    }
+
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+
+        int num = 1;
+
+        //runs through integers and checks if they appear in the array, break if missing.
+        for (int i = 0; i <= array.length; i++) {
+            if(!contains(array, i)){
+                num = i;
+                break;
+            }
+
+        }
+
+        return num;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+
+        int max = array[0];
+        int secondMax = 0;
+        //goes through the array and check the max and second max values.
+        for (int i = 0; i < array.length; i++) {
+
+            if(max <= array[i]){
+                secondMax = max;
+                max = array[i];
+            }
+
+            if(array[i] < max && array[i] > secondMax)
+                secondMax = array[i];
+
+        }
+        return secondMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+
+        //establish bool variables to check whos bigger and for the return value.
+        boolean bigger = (array1.length >= array2.length);
+        boolean ans = true;
+
+        //if array1 is longer, runs through his elements and check if they appear in array2
+        if(bigger) {
+            for (int i = 0; i < array1.length; i++) {
+                ans = contains(array2, array1[i]);
+                if (!ans)
+                    break;
+            }
+        }
+
+        //else runs through array2 elements and check if they appear in array1
+        else{
+
+                for (int i = 0; i < array2.length ; i++) {
+                    ans = contains(array1, array2[i]);
+                    if (!ans)
+                        break;
+                }
+        }
+
+         return ans;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+
+        //assign to booleans whether the first two elements are increasing or decreasing
+        boolean incOrder = array[1] >= array[0];
+        boolean decOrder = array[1] <= array[0];
+
+        //checks if the array continues to increase/ decrease.
+        for (int i = 2; i < array.length; i++) {
+            if (incOrder)
+                incOrder = array[i] >= array[i - 1];
+            if (decOrder)
+                decOrder = array[i] <= array[i - 1];
+        }
+        return (incOrder || decOrder);
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,140 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String s = "Hello World";
+        int[] arr = allIndexOf(s , 'l');
+        ArrayOps.printArr(arr);
+
     }
+
+    //check if a string contains a specific char
+    public static boolean contains(String s , char c){
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == c)
+                return true;
+
+        }
+
+        return false;
+
+    }
+        // remove all of a given char apearences in a given string.
+    public static String removeChar(String s, char c){
+        String ans = "";
+        for (int i = 0; i <s.length(); i++) {
+            char curr = s.charAt(i);
+            ans = curr == c? ans: ans+curr;
+        }
+        return ans;
+    }
+
+    //turns a given string to lower case
+    public static String Lower(String s){
+        String ans ="";
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) >= 97)
+                ans += s.charAt(i);
+            else
+                ans += (char)(s.charAt(i) +32);
+        }
+        return  ans;
+    }
+
+        //turns given char to upper case
+    public static char upper(char c){
+        if(c >= 97)
+            c = (char)(c-32);
+
+        return c;
+    }
+
+    //turns given char to lower case
+    public static char lower(char c){
+        if(c <= 97)
+            c = (char)(c+32);
+
+        return c;
+    }
+
+
+
+
+
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+
+        //establish variables for the return val and vowels
+        String ans = "";
+        String vow ="aeiou";
+
+        //checks every char and adds it to the answer string
+        for (int i = 0; i <string.length() ; i++) {
+            char curr = string.charAt(i);
+
+            if(contains(vow , curr)){
+               ans = (curr >=97) ? ans + (char)(curr-32) : ans + curr;
+            }
+            else {
+                if(curr == ' ')
+                    ans+= ' ';
+                else
+                    ans = (curr >=97)? ans + curr : ans + (char)(curr+32);
+            }
+        }
+        return ans;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase (String s) {
+
+        //establish variables for answer, current index and current char.
+        String ans ="";
+        int j =0;
+        char curr = s.charAt(0);
+
+        //avoid space before the words.
+        while(curr == ' '){
+            j++;
+            curr = s.charAt(j);
+        }
+
+        // adds the first word all in lower case to the answer.
+        while(curr != ' ') {
+            ans +=lower(curr);
+            j++;
+            curr = s.charAt(j);
+        }
+
+        //adds the rest of the words, without spaces and capitilizing first chars.
+        for(int i = j; i<s.length(); i++){
+            curr = s.charAt(i);
+            if(curr != ' ' && s.charAt(i-1) == ' ')
+                ans+= upper(curr);
+            if(curr != ' ' && s.charAt(i-1) != ' ')
+                ans+= lower(curr);
+
+        }
+
+        return ans;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+
+
+    public static int[] allIndexOf (String s, char c) {
+        int count =0;
+        for (int i = 0; i <s.length(); i++) {
+          count = s.charAt(i) == c ? count+1: count;
+        }
+
+        int[] arr = new int[count];
+        int arrIndex = 0;
+        for (int i = 0; i <s.length(); i++) {
+            if(s.charAt(i) == c){
+                arr[arrIndex] = i;
+                arrIndex++;
+            }
+        }
+
+        return arr;
     }
 }
